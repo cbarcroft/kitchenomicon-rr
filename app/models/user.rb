d4d51217254c000f5817af3afde79c296d2c40a2
Chris Barcroft <chrisabarcroft@gmail.com>
2016-08-27 23:28:33 -0700

    Fixed recipe count logic for sidebar

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,17 @@
 class User < ActiveRecord::Base
-  scope :order_by_recipe_count, -> { order(recipes_count: :desc) }
+  has_many :recipes
+  scope :order_by_recipe_count, -> {
+    select("users.id, users.name, count(recipes.id) AS recipe_count").
+    joins(:recipes).
+    group("users.id").
+    order("recipe_count DESC")
+  }
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  has_many :recipes
+
 
 end
