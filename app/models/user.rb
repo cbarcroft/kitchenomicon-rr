7f25a73e15f62ab3b2993b97f0c422288f5ed1ed
Chris Barcroft <chrisabarcroft@gmail.com>
2016-08-28 00:03:37 -0700

    Switched to left join to display all users regardless of recipe count

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ActiveRecord::Base
   has_many :recipes
   scope :order_by_recipe_count, -> {
     select("users.id, users.name, count(recipes.id) AS recipe_count").
-    joins(:recipes).
+    joins('LEFT JOIN recipes ON recipes.user_id = users.id').
     group("users.id").
     order("recipe_count DESC")
   }
