f96eae5feaed1157141db7bd20f2a5096f9e7284
Chris Barcroft <christopher.barcroft@rakuten.com>
2016-08-29 13:01:52 -0700

    Setting production email config for Heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,4 +88,14 @@ Rails.application.configure do
 
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default_url_options = { :host => 'kitchenomicon.herokuapp.com' }
+
+  ActionMailer::Base.smtp_settings = {
+    :address        => 'smtp.sendgrid.net',
+    :port           => '587',
+    :authentication => :plain,
+    :user_name      => ENV['SENDGRID_USERNAME'],
+    :password       => ENV['SENDGRID_PASSWORD'],
+    :domain         => 'kitchenomicon.herokuapp.com',
+    :enable_starttls_auto => true
+  }
 end
