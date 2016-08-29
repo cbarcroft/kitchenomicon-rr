73db31c8274d61f6b98080261873590d4d13e54a
Chris Barcroft <christopher.barcroft@rakuten.com>
2016-08-29 12:57:14 -0700

    Setting production email config for Heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -87,6 +87,5 @@ Rails.application.configure do
   }
 
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.delivery_method = :smtp
   config.action_mailer.default_url_options = { :host => 'kitchenomicon.herokuapp.com' }
 end
