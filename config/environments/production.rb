5c44b463ad83e02f7cf52d848ee9b7bc0a695604
Chris Barcroft <christopher.barcroft@rakuten.com>
2016-08-29 13:04:54 -0700

    Setting production email config for Heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -95,7 +95,7 @@ Rails.application.configure do
     :authentication => :plain,
     :user_name      => ENV['SENDGRID_USERNAME'],
     :password       => ENV['SENDGRID_PASSWORD'],
-    :domain         => 'kitchenomicon.herokuapp.com',
+    :domain         => 'heroku.com',
     :enable_starttls_auto => true
   }
 end
