17e09e470dae07f308e72582877f5b77d1418fcf
Chris Barcroft <christopher.barcroft@rakuten.com>
2016-08-29 14:03:57 -0700

    Added password reset screen, updated horizontal navigation, and added edit button to Show for recipe if current user owns that recipe

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,11 @@ Rails.application.routes.draw do
   end
   devise_for :users
 
+  as :user do
+    get 'users/edit' => 'devise/registrations#edit', :as => 'edit_user_registration'    
+    put 'users' => 'devise/registrations#update', :as => 'user_registration'            
+  end
+
   root to: "recipes#index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
